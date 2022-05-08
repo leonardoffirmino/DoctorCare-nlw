ffec4e30eced0dcb9e78ab82812ba737704ae06a
Leonardo <firmino750@gmail.com>
2022-05-08 18:16:52 -0300

    finish deploy

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopOnScroll()
+  activeteMenuAtCurrentSection(home)
+  activeteMenuAtCurrentSection(services)
+  activeteMenuAtCurrentSection(about)
+  activeteMenuAtCurrentSection(contact)
+}
+
+function activeteMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+
+
 }
 
 function showNavOnScroll() {
